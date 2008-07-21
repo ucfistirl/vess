5a3d93033ae44c35066775cecdc3ff54859dd309
Jason Daly <jdaly@ist.ucf.edu>
2008-07-21 20:20:33 +0000

    Removed all VisualStudio project files to prepare for the SCons switch

--- VessWindows/dummy.c
+++
@@ -1,4 +0,0 @@
-// This file serves no purpose, except to allow the "Compiler" branch of the
-// project properties to be displayed in Visual C++ 2003.  Without this, there
-// is no way to view or change the compiler options, except by modifying the
-// .vcproj file manually.
